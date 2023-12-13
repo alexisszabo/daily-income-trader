08b7093ac95a09f4679e14d2c9ae299a9cf6d18d
Alexis Szabo <alexis@appybara.com>
2023-12-12 19:25:37 -0800

    Skeleton Django ORM Setup
    https://abdus.dev/posts/django-orm-standalone/

---
+++ manage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# https://abdus.dev/posts/django-orm-standalone/
+
+def init_django():
+    import django
+    from django.conf import settings
+
+    if settings.configured:
+        return
+
+    settings.configure(
+        INSTALLED_APPS=[
+            'db',
+        ],
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': 'myapp',
+                'USER': 'myapp_user',
+                'PASSWORD': 'myapp',
+                'HOST': '127.0.0.1',
+                'PORT': '5432',
+            }
+        }
+    )
+    django.setup()
+
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    init_django()
+    execute_from_command_line()
