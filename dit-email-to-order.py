4871e69b20f89421bb940767ca08cd0d97413963
Alexis Szabo <alexis@appybara.com>
2023-12-16 17:08:27 -0800

    Move mail functions to mail.py
    Also cleaned up datetime import to be less confusing

--- dit-email-to-order.py
+++ dit-email-to-order.py
@@ -1,90 +1,18 @@
-# https://github.com/smkent/waffles/blob/70c9d32522adea456216b5f95e541dd9f092e4ba/wafflesbot/jmap.py#L349-L353
-
 import os
 import re
-from datetime import datetime
-from jmapc import (
-    Client,
-    Ref,
-)
-
-from jmapc.methods import (
-    EmailGet,
-    EmailGetResponse,
-    EmailQuery,
-    MailboxChanges,
-    MailboxChangesResponse,
-    MailboxGet,
-    MailboxGetResponse,
-    MailboxQuery,
-    MailboxQueryChanges,
-    MailboxQueryChangesResponse,
-    MailboxQueryResponse,
-    MailboxSet,
-    MailboxSetResponse,
-)
-from jmapc.models import (
-    EmailQueryFilterCondition,
-    MailboxQueryFilterCondition,
-)
-
+import datetime as dt
 from db.models import Order
-
-client = Client.create_with_api_token(
-    host=os.environ["JMAP_HOST"], api_token=os.environ["JMAP_API_TOKEN_DIT"]
+from mail import (
+  get_mailbox,
+  get_mail_client,
+  get_emails_after
 )
 
-methods = [
-    MailboxQuery(filter=MailboxQueryFilterCondition(name=os.environ["JMAP_FOLDER_NAME_DIT"])),
-    MailboxGet(ids=Ref("/ids")),
-]
+client = get_mail_client(os.environ["JMAP_HOST"], os.environ["JMAP_API_TOKEN_DIT"])
+mailbox = get_mailbox(client, os.environ["JMAP_FOLDER_NAME_DIT"])
+emails = get_emails_after(client, mailbox, dt.datetime.today())
+#emails = get_emails_after(client, mailbox, dt.datetime(2023, 12, 12))
 
-# Call JMAP API with the prepared request
-results = client.request(methods)
-
-# Retrieve the InvocationResponse for the second method. The InvocationResponse
-# contains the client-provided method ID, and the result data model.
-method_2_result = results[1]
-
-# Retrieve the result data model from the InvocationResponse instance
-method_2_result_data = method_2_result.response
-
-# Retrieve the Mailbox data from the result data model
-assert isinstance(
-    method_2_result_data, MailboxGetResponse
-), "Error in Mailbox/get method"
-mailboxes = method_2_result_data.data
-
-# Although multiple mailboxes may be present in the results, we only expect a
-# single match for our query. Retrieve the first Mailbox from the list.
-mailbox = mailboxes[0]
-
-# Print some information about the mailbox
-#print(f"Found the mailbox named {mailbox.name} with ID {mailbox.id}")
-#print(
-#    f"This mailbox has {mailbox.total_emails} emails, "
-#    f"{mailbox.unread_emails} of which are unread"
-#)
-
-# Only check for todays emails
-after = datetime.today().replace(hour=0, minute=0, second=0);
-
-# Get the email
-methods = [
-  EmailQuery(
-    filter=EmailQueryFilterCondition(
-      in_mailbox=mailbox.id,
-      after=after,
-    )
-  ),
-  EmailGet(
-    ids=Ref("/ids"),
-    fetch_all_body_values=True
-  )
-]
-results = client.request(methods)
-assert isinstance(results[1].response, EmailGetResponse)
-emails = results[1].response
 n_emails = len(emails.data)
 
 if (n_emails == 1):
@@ -133,7 +61,7 @@ if (n_emails == 1):
 
     if okay_to_write_to_database:
       print('okay to write to database')
-      today = datetime.today()
+      today = dt.datetime.today()
 
       matching_orders = Order.objects.filter(ticker=ticker, date=today)
 
@@ -152,8 +80,3 @@ elif (n_emails > 0):
   print('Found more emails than expected')
 else:
   print('No relevant email found')
-
-# Example output:
-#
-# Found the mailbox named Inbox with ID deadbeef-0000-0000-0000-000000000001
-# This mailbox has 42 emails, 4 of which are unread
---
+++ mail.py
@@ -0,0 +1,71 @@
+# https://github.com/smkent/waffles/blob/70c9d32522adea456216b5f95e541dd9f092e4ba/wafflesbot/jmap.py#L349-L353
+
+from datetime import datetime
+
+from jmapc import (
+    Client,
+    Mailbox,
+    Ref,
+)
+
+from jmapc.methods import (
+    EmailGet,
+    EmailGetResponse,
+    EmailQuery,
+    MailboxGet,
+    MailboxGetResponse,
+    MailboxQuery,
+)
+
+from jmapc.models import (
+    EmailQueryFilterCondition,
+    MailboxQueryFilterCondition,
+)
+
+def get_mail_client(host: str, api_token: str) -> Client:
+  return Client.create_with_api_token(host=host, api_token=api_token)
+
+def get_mailbox(client: Client, name: str) -> Mailbox:
+  methods = [
+      MailboxQuery(filter=MailboxQueryFilterCondition(name=name)),
+      MailboxGet(ids=Ref("/ids")),
+  ]
+
+  # Call JMAP API with the prepared request
+  results = client.request(methods)
+
+  # Retrieve the InvocationResponse for the second method. The InvocationResponse
+  # contains the client-provided method ID, and the result data model.
+  method_2_result = results[1]
+
+  # Retrieve the result data model from the InvocationResponse instance
+  method_2_result_data = method_2_result.response
+
+  # Retrieve the Mailbox data from the result data model
+  assert isinstance(
+      method_2_result_data, MailboxGetResponse
+  ), "Error in Mailbox/get method"
+  mailboxes = method_2_result_data.data
+
+  # Although multiple mailboxes may be present in the results, we only expect a
+  # single match for our query. Retrieve the first Mailbox from the list.
+  return mailboxes[0]
+
+def get_emails_after(client: Client, mailbox: str, date: datetime) -> EmailGetResponse:
+  # Only check for todays emails
+  # Get the email
+  methods = [
+    EmailQuery(
+      filter=EmailQueryFilterCondition(
+        in_mailbox=mailbox.id,
+        after=date.replace(hour=0, minute=0, second=0)
+      )
+    ),
+    EmailGet(
+      ids=Ref("/ids"),
+      fetch_all_body_values=True
+    )
+  ]
+  results = client.request(methods)
+  assert isinstance(results[1].response, EmailGetResponse)
+  return results[1].response
