e94d98ad9a23ee2d3416bf52c502f629897d63cb
Alexis Szabo <alexis@appybara.com>
2023-12-18 21:00:17 -0800

    Refactor dit-email-to-order.py
    - Use main() function
    - Flatten if statements

--- dit-email-to-order.py
+++ dit-email-to-order.py
@@ -8,14 +8,18 @@ from mail import (
   get_emails_after
 )
 
-client = get_mail_client(os.environ["JMAP_HOST"], os.environ["JMAP_API_TOKEN_DIT"])
-mailbox = get_mailbox(client, os.environ["JMAP_FOLDER_NAME_DIT"])
-emails = get_emails_after(client, mailbox, dt.datetime.today())
-#emails = get_emails_after(client, mailbox, dt.datetime(2023, 12, 12))
+def main():
+  client = get_mail_client(os.environ["JMAP_HOST"], os.environ["JMAP_API_TOKEN_DIT"])
+  mailbox = get_mailbox(client, os.environ["JMAP_FOLDER_NAME_DIT"])
+  emails = get_emails_after(client, mailbox, dt.datetime.today())
+  # emails = get_emails_after(client, mailbox, dt.datetime(2023, 12, 12))
 
-n_emails = len(emails.data)
+  n_emails = len(emails.data)
 
-if (n_emails == 1):
+  if (n_emails != 1):
+    print("Found {n_emails} emails, but expected 1")
+    return
+
   email = emails.data[0]
   text_body = email.body_values['1'].value
 
@@ -41,48 +45,53 @@ if (n_emails == 1):
     if match:
       stop_loss_price = float(match.group(1))
 
-  if (
-       ticker != '' and
-       signal_price > 0 and
-       target_price > 0 and
-       stop_loss_price > 0 and
-       stop_loss_price < signal_price < target_price
-     ):
-    print("Parsed Today's Daily Profits Alert Email")
-    print(f"Ticker: ${ticker}")
-    print(f"Signal Price: ${signal_price}")
-    print(f"Target Price: ${target_price}")
-    print(f"Stop Loss Price: ${stop_loss_price}")
+  parsed_values_seem_reasonable = (
+    ticker != '' and
+    signal_price > 0 and
+    target_price > 0 and
+    stop_loss_price > 0 and
+    stop_loss_price < signal_price < target_price
+  )
 
-    # Do some sanity checking to make sure the numbers are reasonable.
-    profit_difference = target_price - signal_price
-    stop_loss_difference = signal_price - stop_loss_price
+  if not parsed_values_seem_reasonable:
+    return
 
-    MINIMUM_RISK_TO_REWARD_RATIO = 2
-    risk_to_reward_ratio = profit_difference / stop_loss_difference
-    exceeds_target_risk_to_reward_ratio = risk_to_reward_ratio >= MINIMUM_RISK_TO_REWARD_RATIO
+  print("Parsed Today's Daily Profits Alert Email")
+  print(f"Ticker: ${ticker}")
+  print(f"Signal Price: ${signal_price}")
+  print(f"Target Price: ${target_price}")
+  print(f"Stop Loss Price: ${stop_loss_price}")
 
-    # TO DO: Add other criteria here:
-    okay_to_write_to_database = exceeds_target_risk_to_reward_ratio
+  # Do some sanity checking to make sure the numbers are reasonable.
+  profit_difference = target_price - signal_price
+  stop_loss_difference = signal_price - stop_loss_price
 
-    if okay_to_write_to_database:
-      print('okay to write to database')
-      today = dt.datetime.today()
+  MINIMUM_RISK_TO_REWARD_RATIO = 2
+  risk_to_reward_ratio = profit_difference / stop_loss_difference
+  exceeds_target_risk_to_reward_ratio = risk_to_reward_ratio >= MINIMUM_RISK_TO_REWARD_RATIO
 
-      matching_orders = Order.objects.filter(ticker=ticker, date=today)
+  # TO DO: Add other criteria here:
+  values_seem_reasonable = exceeds_target_risk_to_reward_ratio
 
-      if matching_orders.count() == 0:
-        print('creating order')
-        order = Order(
-          date=today,
-          ticker=ticker,
-          signal_price=signal_price,
-          target_price=target_price,
-          stop_loss_price=stop_loss_price
-        ) 
-        order.save()
+  if not values_seem_reasonable:
+    return
 
-elif (n_emails > 0):
-  print('Found more emails than expected')
-else:
-  print('No relevant email found')
+  today = dt.datetime.today()
+
+  matching_orders = Order.objects.filter(ticker=ticker, date=today)
+
+  if matching_orders.count() > 0:
+    return
+
+  print('creating order')
+  order = Order(
+    date=today,
+    ticker=ticker,
+    signal_price=signal_price,
+    target_price=target_price,
+    stop_loss_price=stop_loss_price
+  ) 
+  order.save()
+
+if __name__ == "__main__":
+  main()
